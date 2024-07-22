4cb0f3f556e4357548c24ad0bd9e0edb7b607da4
Macbook <macbook@192.168.11.189>
2024-07-22 11:43:01 +0100

    -m

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <iframe className="min-h-screen w-screen" src="https://nashra.framer.website/" ></iframe>
+  );
+}
