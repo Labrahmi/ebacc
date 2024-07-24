d5c9f0f110209cf50470978dbe82914bc7e74887
Macbook <macbook@192.168.1.21>
2024-07-24 20:22:35 +0100

    fix mobile and improve hero UI

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,15 +8,15 @@ export default function Home() {
       <NavBar />
       <div className="flex py-16"></div>
       <PageWrapper>
-        <main className="flex items-center justify-between w-full gap-32 p-8 py-16 group bg-sky-900 rounded-xl text-sky-50">
+        <main className="flex flex-col-reverse md:flex-row items-center justify-between w-full md:gap-32 gap-8 p-8 py-16 group bg-sky-900 rounded-2xl text-sky-50">
           <div className="">
             <h1 className="font-bold text-5xl">Study with your own pace with eBacc</h1>
             <p className="py-2">Join eBacc today and unlock your full academic potential with expert guidance and personalized learning. As a dedicated English Baccalaureate platform, we prepare you for IGCSE success.</p>
           </div>
           <div className="">
-            <div className="w-64 h-64 rounded-xl relative">
+            <div className="w-64 h-64 rounded-2xl relative">
               <Image
-                className="absolute hover:scale-105 transition-all duration-300"
+                className="absolute md:hover:scale-105 transition-all duration-300"
                 src="/7394943-ai.png"
                 alt="Next.js logo"
                 width={500}
@@ -26,14 +26,14 @@ export default function Home() {
           </div>
         </main>
         <div>
-          <div className="flex flex-row-reverse justify-center items-center p-8 gap-2 bg-sky-900 border border-zinc-800 border-opacity-25 rounded-xl shadow-2xl w-fit m-auto relative -top-16">
+          <div className="flex flex-row-reverse justify-center items-center py-8 gap-2 bg-sky-900 rounded-r-2xl relative -top-6">
             <button className='p-2 px-4 rounded border bg-white font-semibold flex justify-center items-center gap-2'>
               <h1 className="">Join Us Now</h1>
               <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="size-6">
                 <path strokeLinecap="round" strokeLinejoin="round" d="M4.26 10.147a60.438 60.438 0 0 0-.491 6.347A48.62 48.62 0 0 1 12 20.904a48.62 48.62 0 0 1 8.232-4.41 60.46 60.46 0 0 0-.491-6.347m-15.482 0a50.636 50.636 0 0 0-2.658-.813A59.906 59.906 0 0 1 12 3.493a59.903 59.903 0 0 1 10.399 5.84c-.896.248-1.783.52-2.658.814m-15.482 0A50.717 50.717 0 0 1 12 13.489a50.702 50.702 0 0 1 7.74-3.342M6.75 15a.75.75 0 1 0 0-1.5.75.75 0 0 0 0 1.5Zm0 0v-3.675A55.378 55.378 0 0 1 12 8.443m-7.007 11.55A5.981 5.981 0 0 0 6.75 15.75v-1.5" />
               </svg>
             </button>
-            <button className='p-2 px-4 flex justify-center items-center gap-2 text-sky-50'><h1>Learn More</h1></button>
+            <button className='p-2 px-4 flex justify-center items-center gap-2 text-sky-200 hover:text-white transition-all'><h1>Learn More</h1></button>
           </div>
         </div>
       </PageWrapper>
@@ -58,7 +58,7 @@ export default function Home() {
 
       {/* <PageWrapper>
         <div className="">
-          <h1 className="text-xl font-semibold">⌘ Hello World, This is me Youssef</h1>
+          <h1 className="text-2xl font-semibold">⌘ Hello World, This is me Youssef</h1>
         </div>
         <div className="space-y-4 my-4">
           <p className="text-zinc-800">
